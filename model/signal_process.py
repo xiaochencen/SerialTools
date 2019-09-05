4e20e540af189647c430fa6295a058a82e7a7599
xiaochencen <844379300@qq.com>
2019-09-05 20:44:46 +0800

    # 修复HEX转十进制时由于高低位情况引发的错误

--- model/signal_process.py
+++ model/signal_process.py
@@ -17,5 +17,10 @@ def transform_data(data, mark, data_len, total_len):
     t_data = []
     for i in data:
         if len(i) == total_len-len(mark):
-            t_data.append(int(i[0:data_len], 16))
+            # 进行转换最高位应该在最左边，重新进行排序
+            # 还有使用re模块的方法
+            data_p = i[0:data_len]
+            data_p = [data_p[i:i+2] for i in range(0, len(data_p), 2)]
+            data_p = ''.join(list((reversed(data_p))))
+            t_data.append(int(data_p, 16))
     return t_data
