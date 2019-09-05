b9a9a5d4c07c54dec3ac0410f6b3865971c91ed6
xiaochencen <844379300@qq.com>
2019-09-05 19:56:35 +0800

    ## 数据过滤 数据转换完成

--- model/signal_process.py
+++ model/signal_process.py
@@ -1,3 +1,21 @@
 '''
 funciton: process receive data
 '''
+
+
+def filter_data(data, mark, data_len, total_len):
+    data = data.split(mark)[1:]
+    f_data = []
+    for i in data:
+        if len(i) == total_len-len(mark):
+            f_data.append(i[0:data_len])
+    return f_data
+
+
+def transform_data(data, mark, data_len, total_len):
+    data = data.split(mark)[1:]
+    t_data = []
+    for i in data:
+        if len(i) == total_len-len(mark):
+            t_data.append(int(i[0:data_len], 16))
+    return t_data
