ffa3eb0fa274c9a635430de364efb55e2e3cc1e4
xiaochencen <844379300@qq.com>
2019-09-06 20:29:43 +0800

    # 同步修改心率过滤功能

--- model/signal_process.py
+++ model/signal_process.py
@@ -3,16 +3,27 @@ funciton: process receive data
 '''
 
 
-def filter_data(data, mark, data_len, total_len):
+def filter_data(data, mark, data_len, total_len, heart_mark, heart_filter):
+    rate = 0
+    if heart_filter:
+        index = data.find(heart_mark)
+        if index != -1:
+            rate = int(data[index:index + 2], 16)
     data = data.split(mark)[1:]
     f_data = []
     for i in data:
         if len(i) == total_len-len(mark):
             f_data.append(i[0:data_len])
-    return f_data
+    return f_data, rate
 
 
-def transform_data(data, mark, data_len, total_len):
+def transform_data(data, mark, data_len, total_len, heart_mark, heart_filter):
+    rate = 0
+    if heart_filter:
+        index = data.find(heart_mark)
+        if index != -1:
+            index += len(heart_mark)
+            rate = int(data[index:index+2], 16)
     data = data.split(mark)[1:]
     t_data = []
     for i in data:
@@ -23,4 +34,4 @@ def transform_data(data, mark, data_len, total_len):
             data_p = [data_p[i:i+2] for i in range(0, len(data_p), 2)]
             data_p = ''.join(list((reversed(data_p))))
             t_data.append(int(data_p, 16))
-    return t_data
+    return t_data, rate
