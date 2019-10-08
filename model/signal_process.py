a2ab8e9372f281e5cf0b573667e107f880731bbe
xiaochencen <844379300@qq.com>
2019-10-08 13:53:45 +0800

    # 改变第二峰值判断条件

--- model/signal_process.py
+++ model/signal_process.py
@@ -54,9 +54,9 @@ def _is_peak(data):
 
 def _is_cross(data):
     # 可以限制起点终点的距离
-    if len(data) != 3:
-        print("---------------------------------------------------"+str(data))
-        return False
+    # if len(data) != 3:
+    #     print("---------------------------------------------------"+str(data))
+    #     return False
     if data[1] == 0:
         # 终点
         if data[0] > 0 and data[2] == 0:
@@ -72,11 +72,110 @@ def _is_cross(data):
 def find_peaks(data, distance: int = 15, width=None, threshold=0):
     # 可能需要对差值2点的卷积处理，将
     index = []
-    flag1 = 0  # 峰值前端标志
-    flag3 = 0
+    flag1 = 0  # 是否进入峰值段阶段
+    flag3 = 0  # 找到峰值标志，可以寻找终点标志
     begin = 0  # 起点标志
     peak_index = 0  # 上一峰值索引
     temp_peak_index = None  # 临时峰值索引
+    dis = 0  # 两峰之间的采样点数
+    len_data = len(data)
+    # 主循环 会少两个点
+    for i in range(1, len_data-1):
+        signal_logging.info("当前索引%d--%d" % (i, data[i]))
+        # 起点终点确认
+        if data[i] == 0:
+            mark = _is_cross(data[i-1:i + 2])
+            # 检测到起点，记录index
+            if mark == 1 and flag1 == 0:
+                signal_logging.debug('检测到起点%d' % i)
+                begin = i
+                flag1 = 1
+            # 检测到终点，并执行最后的宽度检测
+            elif mark == 2 and flag1 == 1:
+                signal_logging.debug('检测到终点%d' % i)
+                flag1 = 0
+                if i - begin > width and flag3 == 1:
+                    # 找到第一个峰值
+                    if peak_index == 0:
+                        if data[temp_peak_index] < 1000:  # 防止过小值屏蔽
+                            temp_peak_index = None
+                            flag3 = 0
+                            continue
+                        peak_index = temp_peak_index
+                        temp_peak_index = None
+                        index.append(peak_index)
+                        flag3 = 0
+                    # 找到第二个峰值 如果percent 达不到要求
+                    # 那么删除前一个值并将当前值作为第一个值重新搜索确认dis
+                    elif dis == 0:
+                        percent = data[peak_index]/data[temp_peak_index] if \
+                            data[peak_index] < data[temp_peak_index] else\
+                            data[temp_peak_index]/data[peak_index]
+                        cur_dis = temp_peak_index - peak_index
+                        # cur_dis 范围设定在心跳40-150/min 基本确定了量程
+                        if percent > 0.5 and 96 > cur_dis > 26:
+                            dis = temp_peak_index-peak_index
+                            peak_index = temp_peak_index
+                            temp_peak_index = None
+                            index.append(peak_index)
+                            flag3 = 0
+                        else:
+                            index.pop()
+                            # 判断第二个峰值是否达到阈值，如果达到则将该值作为新的第一峰
+                            # 否者重新搜索第一峰
+                            if data[temp_peak_index] > 1000:
+                                peak_index = temp_peak_index
+                                index.append(peak_index)
+                            else:
+                                peak_index = 0
+                            temp_peak_index = None
+                            flag3 = 0
+                    # 找到第一第二峰 根据初始化的参数执行后续搜索
+                    else:
+                        percent = data[peak_index] / data[temp_peak_index] if \
+                            data[peak_index] < data[temp_peak_index] else \
+                            data[temp_peak_index] / data[peak_index]
+                        cur_dis = temp_peak_index - peak_index
+                        # 每次心跳点变化 以及 前后两个峰值 不能超过一定的阈值
+                        if 0.6 * dis < cur_dis < 2 * dis and percent > 0.35:
+                            # todo 确认该点确实是心跳点  可调整加权数据
+                            dis = 0.8 * dis + 0.2 * cur_dis
+                            signal_logging.debug('-----------%d' % dis)
+                            peak_index = temp_peak_index
+                            temp_peak_index = None
+                            index.append(peak_index)
+                            flag3 = 0
+                            pass
+                        else:
+                            # todo 不认为该点是心跳点
+                            temp_peak_index = None
+                            flag3 = 0
+                            pass
+            else:
+                continue
+        # 峰值确认
+        elif data[i] > threshold:
+            if _is_peak(data[i-1:i+2]) and flag1 == 1:
+                # 检测到峰值点，确认峰值距离
+                if i-peak_index > distance:
+                    flag3 = 1
+                    if temp_peak_index is None:
+                        temp_peak_index = i
+                    # 确认找到峰值，并判断是否为全局最优峰值
+                    if temp_peak_index is not None and data[temp_peak_index] < data[i]:
+                        temp_peak_index = i
+    return index
+
+
+def __find_peaks(data, distance: int = 15, width=None, threshold=0):
+    # 可能需要对差值2点的卷积处理，将
+    index = []
+    flag1 = 0  # 是否进入峰值段阶段
+    flag3 = 0  # 找到峰值标志，可以寻找终点标志
+    begin = 0  # 起点标志
+    peak_index = 0  # 上一峰值索引
+    temp_peak_index = None  # 临时峰值索引
+    dis = 0  # 两峰之间的采样点数
     len_data = len(data)
     # 主循环 会少两个点
     for i in range(1, len_data-1):
@@ -93,12 +192,49 @@ def find_peaks(data, distance: int = 15, width=None, threshold=0):
                 signal_logging.debug('检测到终点%d' % i)
                 flag1 = 0
                 if i - begin > width and flag3 == 1:
-                    signal_logging.debug('峰值%d' % temp_peak_index)
-                    peak_index = temp_peak_index
-                    temp_peak_index = None
-                    index.append(peak_index)
-                    flag3 = 0
-            # 掠过少数零点（单零点）
+                    # 找到第一个峰值
+                    if peak_index == 0:
+                        peak_index = temp_peak_index
+                        temp_peak_index = None
+                        index.append(peak_index)
+                        flag3 = 0
+                    # 找到第二个峰值 如果percent 达不到要求
+                    # 那么删除前一个值并将当前值作为第一个值重新搜索确认dis
+                    elif dis == 0:
+                        percent = data[peak_index]/data[temp_peak_index] if \
+                            data[peak_index] < data[temp_peak_index] else\
+                            data[temp_peak_index]/data[peak_index]
+                        cur_dis = temp_peak_index - peak_index
+                        if percent > 0.5 and 96 > cur_dis > 30:
+                            dis = temp_peak_index-peak_index
+                            peak_index = temp_peak_index
+                            temp_peak_index = None
+                            index.append(peak_index)
+                            flag3 = 0
+                        else:
+                            index.pop()
+                            peak_index = temp_peak_index
+                            index.append(peak_index)
+                            temp_peak_index = None
+                            flag3 = 0
+                    else:
+                        div_data = [data[peak_index], data[temp_peak_index]]
+                        cur_dis = temp_peak_index - peak_index
+                        percent = min(div_data) / max(div_data)  # 计算峰值差距
+                        if percent > 0.4 and 0.5 * dis < cur_dis < 1.5 * dis:
+                            # todo 确认该点确实是心跳点  可调整加权数据
+                            dis = 0.8 * dis + 0.2 * cur_dis
+                            signal_logging.debug('----%d' % dis)
+                            peak_index = temp_peak_index
+                            temp_peak_index = None
+                            index.append(peak_index)
+                            flag3 = 0
+                            pass
+                        else:
+                            # todo 不认为该点是心跳点
+                            temp_peak_index = None
+                            flag3 = 0
+                            pass
             else:
                 continue
         elif data[i] > threshold:
@@ -112,9 +248,6 @@ def find_peaks(data, distance: int = 15, width=None, threshold=0):
                     if temp_peak_index is not None and data[temp_peak_index] < data[i]:
                         temp_peak_index = i
     return index
-
-
-
 
 
 
